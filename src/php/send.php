49aecc33c19f4c5413e7441a432d33a218f30376
Alexey <teos.nl@gmail.com>
2017-05-05 18:18:51 +0300

    add mk

--- src/php/send.php
+++ src/php/send.php
@@ -1,5 +1,5 @@
 <?php
-$sendto = "alex_z@franch5.ru, zvnfranch5@yandex.ru, franchaizing-5@yandex.ru";
+$sendto = "alex_z@franch5.ru, zvnfranch5@yandex.ru, franchaizing-5@yandex.ru, royalcours@yandex.ru";
 $phone = nl2br($_POST['phone']);
 $name = nl2br($_POST['name']);
 $email = nl2br($_POST['email']);
